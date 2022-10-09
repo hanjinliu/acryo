simulator rotation bug fix

acryo/alignment/_utils.py
@@ -108,7 +108,7 @@ def lowpass_filter_ft(img: np.ndarray, cutoff: float, order: int = 2) -> np.ndar
         high_pass=False,
         real=False,
     )
-    return weight * fftn(img)
+    return weight * fftn(img)  # type: ignore
 
 
 def lowpass_filter(img: np.ndarray, cutoff: float, order: int = 2) -> np.ndarray:
@@ -138,12 +138,12 @@ def _get_ND_butterworth_filter(
         axis = np.arange(-(d - 1) // 2, (d - 1) // 2 + 1, dtype=np.float32) / (
             d * cutoff
         )
-        ranges.append(np.fft.ifftshift(axis ** 2))
+        ranges.append(np.fft.ifftshift(axis**2))
     if real:
         limit = shape[-1] // 2 + 1
         ranges[-1] = ranges[-1][:limit]
     q2 = reduce(np.add, np.meshgrid(*ranges, indexing="ij", sparse=True))
-    wfilt = 1 / (1 + q2 ** order)
+    wfilt = 1 / (1 + q2**order)
     if high_pass:
         wfilt = 1 - wfilt
     return wfilt
@@ -218,7 +218,9 @@ def _upsampled_dft(
     dim_properties = list(zip(data.shape, upsampled_region_sizes, axis_offsets))
 
     for (n_items, ups_size, ax_offset) in dim_properties[::-1]:
-        kernel = (np.arange(ups_size) - ax_offset)[:, np.newaxis] * np.fft.fftfreq(
+        kernel = (np.arange(ups_size) - ax_offset)[
+            :, np.newaxis
+        ] * np.fft.fftfreq(  # type: ignore
             n_items, upsample_factor
         )
         kernel = np.exp(-2j * np.pi * kernel)
@@ -228,7 +230,7 @@ def _upsampled_dft(
 
 
 def abs2(a: np.ndarray) -> np.ndarray:
-    return a.real ** 2 + a.imag ** 2  # type: ignore
+    return a.real**2 + a.imag**2  # type: ignore
 
 
 def crop_by_max_shifts(power: np.ndarray, left, right):
@@ -253,7 +255,7 @@ def _draw_ncc_landscape_no_crop(
     if max_shifts is not None:
         max_shifts = tuple(max_shifts)
     pad_width, sl = _get_padding_params(img0.shape, img1.shape, max_shifts)
-    padimg = np.pad(img0[sl], pad_width=pad_width, mode="constant", constant_values=0)
+    padimg = np.pad(img0[sl], pad_width=pad_width, mode="constant", constant_values=0)  # type: ignore
 
     corr = fftconvolve(padimg, img1[(slice(None, None, -1),) * ndim], mode="valid")[
         (slice(1, -1, None),) * ndim
@@ -261,13 +263,13 @@ def _draw_ncc_landscape_no_crop(
 
     _win_sum = _window_sum_2d if ndim == 2 else _window_sum_3d
     win_sum1 = _win_sum(padimg, img1.shape)
-    win_sum2 = _win_sum(padimg ** 2, img1.shape)
+    win_sum2 = _win_sum(padimg**2, img1.shape)
 
     template_mean = np.mean(img1)
     template_volume = np.prod(img1.shape)
     template_ssd = np.sum((img1 - template_mean) ** 2)
 
-    var = (win_sum2 - win_sum1 ** 2 / template_volume) * template_ssd
+    var = (win_sum2 - win_sum1**2 / template_volume) * template_ssd
 
     # zero division happens when perfectly matched
     response = np.zeros_like(corr)
@@ -327,11 +329,11 @@ def subpixel_zncc(
 # Identical to skimage.feature.template, but compatible between numpy and cupy.
 def _window_sum_2d(image, window_shape):
     window_sum = np.cumsum(image, axis=0)
-    window_sum = window_sum[window_shape[0] : -1] - window_sum[: -window_shape[0] - 1]
+    window_sum = window_sum[window_shape[0] : -1] - window_sum[: -window_shape[0] - 1]  # type: ignore
     window_sum = np.cumsum(window_sum, axis=1)
     window_sum = (
         window_sum[:, window_shape[1] : -1] - window_sum[:, : -window_shape[1] - 1]
-    )
+    )  # type: ignore
 
     return window_sum
 
@@ -342,7 +344,7 @@ def _window_sum_3d(image, window_shape):
     window_sum = (
         window_sum[:, :, window_shape[2] : -1]
         - window_sum[:, :, : -window_shape[2] - 1]
-    )
+    )  # type: ignore
 
     return window_sum
 
@@ -391,9 +393,9 @@ def _create_mesh(
         shifts = np.array(maxima, dtype=np.float32) - np.array(
             midpoints, dtype=np.float32
         )  # type: ignore
-        max_shifts = np.array(max_shifts, dtype=np.float32)  # type: ignore
-        left = -shifts - max_shifts
-        right = -shifts + max_shifts
+        _max_shifts = np.array(max_shifts, dtype=np.float32)  # type: ignore
+        left = -shifts - _max_shifts
+        right = -shifts + _max_shifts
         local_shifts = tuple(
             [
                 int(np.round(max(shiftl, -1) * upsample_factor)),

acryo/classification/pca.py
@@ -6,6 +6,7 @@ if TYPE_CHECKING:
     from matplotlib.axes import Axes
     from sklearn.decomposition import PCA
     from sklearn.cluster import KMeans
+    from typing_extensions import Self
 
 
 class PcaClassifier:
@@ -43,18 +44,18 @@ class PcaClassifier:
     def kmeans(self) -> KMeans:
         return self._kmeans
 
-    def run(self):
+    def run(self) -> Self:
         self._pca.fit(self._image_flat(mask=True))
         self._labels = self._kmeans.fit_predict(self.get_transform())
         return self
 
     def transform(self, input: np.ndarray, mask: bool = True) -> np.ndarray:
         if mask:
-            input = input * mask
+            input = input * self._mask
         flat = input.reshape(input.shape[0], -1)
         return self._pca.transform(flat)
 
-    def predict(self, input) -> np.ndarray:
+    def predict(self, input: np.ndarray) -> np.ndarray:
         transformed = self.transform(input)
         labels = self._kmeans.predict(transformed)
         return labels

acryo/simulator.py
@@ -16,7 +16,7 @@ from .molecules import Molecules
 from . import _utils
 
 if TYPE_CHECKING:
-    from typing_extensions import Self
+    from typing_extensions import Self, Literal
     from scipy.spatial.transform import Rotation
 
 
@@ -28,7 +28,7 @@ class Component(NamedTuple):
 class TomogramSimulator:
     def __init__(
         self,
-        order: int = 3,
+        order: Literal[0, 1, 3] = 3,
         scale: nm = 1.0,
         corner_safe: bool = False,
     ) -> None:
@@ -37,7 +37,7 @@ class TomogramSimulator:
             raise ValueError(
                 f"The third argument 'order' must be 0, 1 or 3, got {order!r}."
             )
-        self._order = order
+        self._order: Literal[0, 1, 3] = order
 
         # check scale
         self._scale = float(scale)
@@ -57,7 +57,7 @@ class TomogramSimulator:
         return MappingProxyType(self._components)
 
     @property
-    def order(self) -> int:
+    def order(self) -> Literal[0, 1, 3]:
         """Return the interpolation order."""
         return self._order
 
@@ -72,7 +72,7 @@ class TomogramSimulator:
 
     def replace(
         self,
-        order: int | None = None,
+        order: Literal[0, 1, 3] | None = None,
         scale: float | None = None,
         corner_safe: bool | None = None,
     ) -> Self:
@@ -169,6 +169,9 @@ class TomogramSimulator:
         ----------
         shape : tuple of int
             Shape of the tomogram.
+        tilt_range : tuple of float, optional
+            Range of tilt series in degrees, such as (-60., 60.). This argument is
+            used to mask missing wedge in the tomogram.
 
         Returns
         -------
@@ -177,15 +180,18 @@ class TomogramSimulator:
         """
         tomogram = np.zeros(shape, dtype=np.float32)
         for mol, image in self._components.values():
+            # image slice must be integer so split it into two parts
             pos = mol.pos / self._scale
             intpos = pos.astype(np.int32)
             residue = pos - intpos.astype(np.float32)
+
+            # construct matrices
             template_shape = image.shape
             center = (np.array(template_shape) - 1.0) / 2.0
             starts = intpos - center.astype(np.int32)
             stops = starts + template_shape
             mtxs = _compose_affine_matrices(
-                center, mol.rotator, output_center=center + residue
+                center, mol.rotator.inv(), output_center=center + residue
             )
 
             # prefilter here to avoid repeated computation
@@ -194,18 +200,20 @@ class TomogramSimulator:
 
             for start, stop, mtx in zip(starts, stops, mtxs):
                 # To avoid out-of-boundary, we need to clip the start and stop
-                sl_src: list[slice] = []
-                sl_dst: list[slice] = []
+                sl_src_list: list[slice] = []
+                sl_dst_list: list[slice] = []
                 for s, e, size, tsize in zip(start, stop, shape, template_shape):
                     _sl, _pads, _out_of_bound = _utils.make_slice_and_pad(s, e, size)
-                    sl_dst.append(_sl)
+                    sl_dst_list.append(_sl)
                     if _out_of_bound:
                         s0, s1 = _pads
-                        sl_src.append(slice(s0, tsize - s1))
+                        sl_src_list.append(slice(s0, tsize - s1))
                     else:
-                        sl_src.append(slice(None))
+                        sl_src_list.append(slice(None))
+                sl_src = tuple(sl_src_list)
+                sl_dst = tuple(sl_dst_list)
 
-                tomogram[tuple(sl_dst)] += ndi.affine_transform(
+                tomogram[sl_dst] += ndi.affine_transform(
                     image,
                     mtx,
                     mode="constant",
@@ -213,10 +221,11 @@ class TomogramSimulator:
                     order=self.order,
                     prefilter=False,
                 )[
-                    tuple(sl_src)
+                    sl_src
                 ]  # type: ignore
 
         if tilt_range is not None:
+            # Remove the missing wedge from the Fourier space
             rot = Rotation.identity()
             mask = _utils.missing_wedge_mask(rot, tilt_range, tomogram.shape)
             ft = fftn(tomogram) * mask  # type: ignore