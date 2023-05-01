fix dask computation

acryo/_dask.py
@@ -1,7 +1,9 @@
 # pyright: reportPrivateImportUsage=false
 from __future__ import annotations
+from abc import ABC, abstractmethod
 from typing import (
     Any,
+    List,
     Callable,
     Generic,
     Iterable,
@@ -9,9 +11,13 @@ from typing import (
     TYPE_CHECKING,
     Iterator,
     Sequence,
+    MutableSequence,
     overload,
 )
 from typing_extensions import ParamSpec, Self, Concatenate
+
+import numpy as np
+from numpy.typing import NDArray
 
 if TYPE_CHECKING:
     from dask import array as da
@@ -19,6 +25,7 @@ if TYPE_CHECKING:
 
 _P = ParamSpec("_P")
 _R = TypeVar("_R")
+_D = TypeVar("_D", bound=np.generic)
 
 
 class DaskTaskIterator(Generic[_R]):
@@ -32,19 +39,28 @@ class DaskTaskIterator(Generic[_R]):
         return DaskTaskList(self._iter)
 
 
-class DaskTaskList(Generic[_R]):
+class _DaskComputable(Generic[_R], ABC):
+    @abstractmethod
+    def _as_dask_list(self) -> list[Any]:
+        """Convert to a list that is ready for dask computation"""
+
+    def compute(self) -> list[_R]:
+        from dask import compute
+
+        return compute(self._as_dask_list())[0]
+
+
+class DaskTaskList(_DaskComputable[_R]):
     def __init__(self, tasks: Iterable[da.Array | Delayed]) -> None:
         self._tasks = list(tasks)
 
     def count(self) -> int:
         return len(self._tasks)
 
-    def compute(self) -> list[_R]:
-        from dask import compute
+    def _as_dask_list(self) -> list[Any]:
+        return self._tasks
 
-        return compute(self._tasks)[0]
-
-    def asarrays(self, shape: tuple[int, ...], dtype: Any) -> DaskArrayList:
+    def asarrays(self, shape: tuple[int, ...], dtype: type[_D]) -> DaskArrayList[_D]:
         from dask import array as da
 
         return DaskArrayList(
@@ -84,7 +100,35 @@ class DaskTaskPool(DaskTaskList[_R], Generic[_P, _R]):
         return self
 
 
-class DaskArrayList(Sequence["da.Array"]):
+class NestedDaskTaskList(
+    MutableSequence[_DaskComputable[_R]], _DaskComputable[List[_R]]
+):
+    def __init__(self, tasks: Iterable[_DaskComputable[_R]]) -> None:
+        self._tasks = list(tasks)
+
+    def insert(self, index: int, task: _DaskComputable) -> None:
+        return self._tasks.insert(index, task)
+
+    def __getitem__(self, index: int) -> _DaskComputable:
+        return self._tasks[index]
+
+    def __setitem__(self, index: int, task: _DaskComputable) -> None:
+        self._tasks[index] = task
+
+    def __delitem__(self, index: int) -> None:
+        del self._tasks[index]
+
+    def __iter__(self) -> Iterator[_DaskComputable]:
+        return iter(self._tasks)
+
+    def __len__(self) -> int:
+        return len(self._tasks)
+
+    def _as_dask_list(self) -> list[Any]:
+        return [t._as_dask_list() for t in self._tasks]
+
+
+class DaskArrayList(Sequence["da.Array"], _DaskComputable[NDArray[_D]]):
     def __init__(self, arrays: Iterable[da.Array]):
         self._arrays = list(arrays)
 
@@ -96,11 +140,11 @@ class DaskArrayList(Sequence["da.Array"]):
 
     @overload
     def __getitem__(self, index: int) -> da.Array:
-        ...  # fmt: skip
+        ...
 
     @overload
     def __getitem__(self, index: slice) -> list[da.Array]:
-        ...  # fmt: skip
+        ...
 
     def __getitem__(self, index):
         return self._arrays[index]
@@ -111,10 +155,8 @@ class DaskArrayList(Sequence["da.Array"]):
 
         return cls(itertools.chain(*obj))
 
-    def compute(self) -> list[da.Array]:
-        from dask import compute
-
-        return compute(self)[0]
+    def _as_dask_list(self) -> list[Any]:
+        return self._arrays
 
     def as_stack(self, axis: int = 0) -> da.Array:
         from dask import array as da
@@ -133,3 +175,36 @@ class DaskArrayList(Sequence["da.Array"]):
 
     def enumerate(self) -> Iterator[tuple[int, da.Array]]:
         return enumerate(self)
+
+
+_R1 = TypeVar("_R1")
+_R2 = TypeVar("_R2")
+
+
+@overload
+def compute(arg: _DaskComputable[_R]) -> list[_R]:
+    ...
+
+
+@overload
+def compute(
+    arg: tuple[_DaskComputable[_R], _DaskComputable[_R1]]
+) -> tuple[list[_R], list[_R1]]:
+    ...  # fmt: skip
+
+
+@overload
+def compute(
+    arg: tuple[_DaskComputable[_R], _DaskComputable[_R1], _DaskComputable[_R2]]
+) -> tuple[list[_R], list[_R1], list[_R2]]:
+    ...  # fmt: skip
+
+
+def compute(arg: _DaskComputable[_R] | tuple[_DaskComputable[_R], ...]):
+    """Compute dask tasks"""
+    from dask import compute
+
+    if isinstance(arg, _DaskComputable):
+        return arg.compute()
+
+    return tuple(compute([a._as_dask_list() for a in arg])[0])

acryo/alignment/_base.py
@@ -26,7 +26,7 @@ from acryo._utils import (
     lowpass_filter_ft,
 )
 from acryo._typed_scipy import ifftn, spline_filter, affine_transform, map_coordinates
-from acryo._dask import DaskTaskPool
+from acryo._dask import DaskTaskPool, compute
 from ._bound import ParametrizedModel
 
 if TYPE_CHECKING:
@@ -647,8 +647,6 @@ class RotationImplemented(BaseAlignmentModel):
             )
             cval = float(np.percentile(self._template, 1))
             if self._n_templates > 1:
-                # all_templates: list[da.Array] = []
-                # all_masks: list[da.Array] = []
                 inputs_templates: list[NDArray[np.float32]] = [
                     spline_filter(
                         tmp * self._mask,
@@ -666,37 +664,9 @@ class RotationImplemented(BaseAlignmentModel):
                         pool_template.add_task(
                             tmp, mat, order=3, cval=cval, prefilter=False
                         )
-                        # all_templates.append(
-                        #     da.from_delayed(
-                        #         self._transform_template_delayed(
-                        #             tmp, mat, order=3, cval=cval, prefilter=False
-                        #         ),
-                        #         shape=tmp.shape,
-                        #         dtype=tmp.dtype,
-                        #     )
-                        # )
                     pool_mask.add_tasks(
                         ntmp, self._mask, mat, order=3, mode="nearest", prefilter=False
                     )
-
-                    # all_masks.extend(
-                    #     [
-                    #         delayed_affine(
-                    #             self._mask,
-                    #             mat,
-                    #             order=3,
-                    #             mode="nearest",
-                    #             prefilter=False,
-                    #         )
-                    #     ]
-                    #     * len(inputs_templates)
-                    # )
-                template_input = np.stack(pool_template.compute(), axis=0)
-                mask_input = np.stack(pool_mask.compute(), axis=0)
-                # template_input, mask_input = da.compute(
-                #     da.stack(all_templates, axis=0), da.stack(all_masks, axis=0)
-                # )
-
             else:
                 template_masked = spline_filter(
                     self._template * self._mask,
@@ -713,8 +683,15 @@ class RotationImplemented(BaseAlignmentModel):
                     pool_mask.add_task(
                         self._mask, mat, order=3, mode="nearest", prefilter=False
                     )
-                template_input = np.stack(pool_template.compute(), axis=0)
-                mask_input = np.stack(pool_mask.compute(), axis=0)
+
+            _templates, _masks = compute(
+                (
+                    pool_template.asarrays(self.input_shape, dtype=np.complex64),
+                    pool_mask.asarrays(self.input_shape, dtype=np.float32),
+                )
+            )
+            template_input = np.stack(_templates, axis=0)
+            mask_input = np.stack(_masks, axis=0)
         else:
             pool = DaskTaskPool.from_func(self.pre_transform)
             if self._n_templates > 1: