fix fit() without rotation

acryo/_rotation.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 import itertools
-from typing import Callable, Literal, Sequence
+from typing import Callable, Literal, Sequence, cast
 import numpy as np
+from numpy.typing import NDArray
 from scipy.spatial.transform import Rotation
 
 from acryo._types import Ranges, RangeLike, RotationType
@@ -17,14 +18,34 @@ def _normalize_a_range(rng: RangeLike) -> RangeLike:
 
 
 def _normalize_ranges(rng: RangeLike | Ranges) -> Ranges:
-    if isinstance(rng, (tuple, list)) and isinstance(rng[0], tuple):
+    if np.array(rng).ndim == 2:
         return tuple(_normalize_a_range(r) for r in rng)  # type: ignore
     else:
         rng_ = _normalize_a_range(rng)  # type: ignore
         return (rng_,) * 3
 
 
-def normalize_rotations(rotations: RotationType | None) -> np.ndarray:
+def _seq_of_max_and_step_to_quat(rotations: RangeLike | Ranges) -> NDArray[np.float32]:
+    _rotations = _normalize_ranges(rotations)
+    angles = []
+    for max_rot, step in _rotations:
+        if step == 0:
+            angles.append(np.zeros(1))
+        else:
+            n = int(max_rot / step)
+            angles.append(np.linspace(-n * step, n * step, 2 * n + 1))
+
+    _quat: list[NDArray[np.float32]] = []
+    for angs in itertools.product(*angles):
+        _quat.append(
+            from_euler_xyz_coords(np.array(angs), "zyx", degrees=True)
+            .as_quat(canonical=False)
+            .astype(np.float32, copy=False)
+        )
+    return np.stack(_quat, axis=0)
+
+
+def normalize_rotations(rotations: RotationType | None) -> NDArray[np.floating]:
     """
     Normalize various rotation expressions to quaternions.
 
@@ -41,25 +62,22 @@ def normalize_rotations(rotations: RotationType | None) -> np.ndarray:
     if isinstance(rotations, Rotation):
         quats = rotations.as_quat(canonical=False)
     elif rotations is not None:
-        _rotations = _normalize_ranges(rotations)
-        angles = []
-        for max_rot, step in _rotations:
-            if step == 0:
-                angles.append(np.zeros(1))
-            else:
-                n = int(max_rot / step)
-                angles.append(np.linspace(-n * step, n * step, 2 * n + 1))
-
-        _quat: list[np.ndarray] = []
-        for angs in itertools.product(*angles):
-            _quat.append(
-                from_euler_xyz_coords(np.array(angs), "zyx", degrees=True).as_quat(
-                    canonical=False
-                )
+        if not hasattr(rotations, "__iter__"):
+            raise TypeError("rotations must be iterable")
+        list_rot = list(rotations)
+        if isinstance(list_rot[0], Rotation):
+            list_rot = cast("list[Rotation]", list_rot)
+            quats = np.stack(
+                [
+                    r.as_quat(canonical=False).astype(np.float32, copy=False)
+                    for r in list_rot
+                ],
+                axis=0,
             )
-        quats = np.stack(_quat, axis=0)
+        else:
+            quats = _seq_of_max_and_step_to_quat(rotations)
     else:
-        quats = np.array([[0.0, 0.0, 0.0, 1.0]])
+        quats = np.array([[0.0, 0.0, 0.0, 1.0]], dtype=np.float32)
 
     return quats
 

acryo/alignment/_base.py
@@ -581,8 +581,8 @@ class RotationImplemented(BaseAlignmentModel):
         self,
         img: NDArray[np.float32],
         max_shifts: tuple[subpixel, subpixel, subpixel],
-        quaternion: NDArray[np.float32] | None,
-        pos: NDArray[np.float32] | None,
+        quaternion: NDArray[np.float32] | None = None,
+        pos: NDArray[np.float32] | None = None,
         backend: Backend | None = None,
     ) -> AlignmentResult:
         """
@@ -636,6 +636,10 @@ class RotationImplemented(BaseAlignmentModel):
         pos = np.zeros(3, dtype=np.float32)
         img_input = xp.asarray(img)
         _template, _mask = self._get_template_and_mask_input(backend=xp)
+        if _template.ndim == 3:
+            _template = [_template]
+        if _mask.ndim == 3:
+            _mask = [_mask]
         for quat, tmp, mask in zip(self.quaternions, _template, _mask):
             pool.add_task(
                 self.pre_transform(img_input * mask, xp),